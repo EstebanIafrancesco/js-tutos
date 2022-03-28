9016cf9ed70a57c6eb4a4d2728ad8c0c1c52c2c2
Esteban Iafrancesco <eiafrancescoa@dane.gov.co>
2022-03-28 16:11:41 -0500

    fibonacci series

---
+++ js-problems/fibonacci.js
@@ -0,0 +1,12 @@
+// function that returns a fibonacci series
+// the series works like this 0,1,1,2,3,5,8,13,21,34,55,89.....
+let fiboSeed = [0,1];
+
+function fibonacci(n){
+    for (let i = 2; i <= n; i++) {
+        fiboSeed.push(fiboSeed[i-1]+fiboSeed[i-2]);
+    }
+    return fiboSeed;
+}
+
+console.log(fibonacci(5));
